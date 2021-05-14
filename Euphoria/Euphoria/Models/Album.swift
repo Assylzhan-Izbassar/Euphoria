4a3acc6f2e6c9ea20bec92063cf87d6ccd4e8a34
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-05-14 10:57:40 +0600

    interface_dev

---
+++ Euphoria/Euphoria/Models/Album.swift
@@ -0,0 +1,28 @@
+//
+//  Album.swift
+//  Euphoria
+//
+//  Created by macbook on 14.05.2021.
+//
+
+import Foundation
+
+struct Album {
+    var name: String
+    var songs: [Song]
+}
+
+extension Album {
+    static func getAlbums() -> [Album] {
+        return [
+            Album(name: "For training", songs: [
+            ]),
+            Album(name: "For stady", songs: [
+            ]),
+            Album(name: "Sadly", songs: [
+            ]),
+            Album(name: "Dance", songs: [
+            ])
+        ]
+    }
+}
---
+++ Euphoria/Euphoria/Models/Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  Euphoria
+//
+//  Created by macbook on 14.05.2021.
+//
+
+import Foundation
+
+struct Song {
+    var name: String
+    var artistName: String
+    var posterUrl: String
+    var fileName: String
+}
