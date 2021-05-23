3c3011c89e2f44820e0fe07f036c2f4a9253950b
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-05-23 19:15:18 +0600

    upd_TabBarViewController

---
+++ Euphoria/Euphoria/ViewController/TabBarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarViewController.swift
+//  Euphoria
+//
+//  Created by macbook on 23.05.2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
